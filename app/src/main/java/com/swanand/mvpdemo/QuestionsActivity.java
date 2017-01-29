cc0c3a07d9f417ea087d0b5cc379260cbb0e6bfb
Swanand Keskar <sw4n4nd@gmail.com>
2017-01-29 12:02:59 +0530

    code cleanup

--- app/src/main/java/com/swanand/mvpdemo/QuestionsActivity.java
+++ app/src/main/java/com/swanand/mvpdemo/QuestionsActivity.java
@@ -6,15 +6,12 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.TextView;
 
 import com.swanand.mvpdemo.adapters.QuestionsAdapter;
 import com.swanand.mvpdemo.model.Item;
 import com.swanand.mvpdemo.presenter.QuestionsPresenter;
 import com.swanand.mvpdemo.view.QuestionsView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionsActivity extends AppCompatActivity implements QuestionsView {
@@ -22,16 +19,16 @@ public class QuestionsActivity extends AppCompatActivity implements QuestionsVie
     ProgressDialog progressDialog;
     QuestionsAdapter questionsAdapter;
     RecyclerView questionsRecyclerview;
-    TextView test;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_questions);
 
         initRecyclerView();
         questionsPresenter=new QuestionsPresenter(this);
         questionsPresenter.presentQuestions();
-        test= (TextView) findViewById(R.id.test);
+
     }
 
     public void initRecyclerView(){
@@ -44,7 +41,7 @@ public class QuestionsActivity extends AppCompatActivity implements QuestionsVie
 
     @Override
     public void renderData(List<Item> items) {
-        Log.d("data",items.toString());
+
         questionsAdapter.addQuestions(items);
         questionsAdapter.notifyDataSetChanged();
 
